7da13784f499ef680266887d50bd4d2c998f3ff7
David <davidm2556@gmail.com>
2017-12-07 21:12:23 -0500

    Work in progress, drivetrain subsystem

--- 2018_Programming_Class/src/org/team2168/RobotMap.java
+++ 2018_Programming_Class/src/org/team2168/RobotMap.java
@@ -6,6 +6,8 @@ package org.team2168;
  * floating around.
  */
 public class RobotMap {
+	
+	
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
@@ -15,4 +17,25 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+
+	
+	////////////////////////////////////Robot Map Wiring/////////////////////////////////////////////////////
+	
+	
+	
+	//Joystics////////////////////////////////////////////////////////////
+	
+	//PWM 0-9////////////////////////////////////////////////////////////
+	public static final int  LEFT_MOTOR_1 = 0;
+	public static final int  LEFT_MOTOR_2 = 1;
+	public static final int  LEFT_MOTOR_3 = 2;
+	
+	public static final int  RIGHT_MOTOR_1 = 3;
+	public static final int  RIGHT_MOTOR_2 = 4;
+	public static final int  RIGHT_MOTOR_3 = 5;
+	//PWM 10-19/////////////////////////////////////////////////////////
+	
+	
+	//drivetrain///////////////////////////////////////////////////////
+	public static final boolean REVERSE_LEFT_1_MOTOR_DIRECTION = false;
 }
